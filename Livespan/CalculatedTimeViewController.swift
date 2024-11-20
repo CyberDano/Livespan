ebda4e12a865e6e97091a0e628549768adbef7b1
DanoMAC <dano@cev.com>
2024-11-20 11:04:58 +0100

    App finished + some details

--- Livespan/CalculatedTimeViewController.swift
+++ Livespan/CalculatedTimeViewController.swift
@@ -1,8 +1,40 @@
 import UIKit
 
 class CalculatedTimeViewController: UIViewController {
+    let data = UserDefaults.standard
+    let averageAge: Int = 150672 // esperanza de vida promedio a partir de 65 años en horas
+    @IBOutlet weak var CalculatedTime: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Llega a la segunda pantalla")
+        print(data.float(forKey: "age"))
+        print(data.float(forKey: "work"))
+        print(data.float(forKey: "sleep"))
+        let time = StimateHours()
+        if time > 0 {
+            CalculatedTime.text += "\(time) hours (\(Float(time/24).roundToDecimal(1)) days) left of free time to enjoy :)"
+        } else {
+            CalculatedTime.text += "all the time you want to enjoy :)"
+        }
+    }
+    
+    func StimateHours() -> Float {
+        var agetime: Float = (data.float(forKey: "age") * 365) * 24 //tu edad en horas
+        var worktime: Float = (24/data.float(forKey: "work")) // tiempo del día de trabajo
+        var sleeptime: Float = (24/data.float(forKey: "sleep")) // tiempo del día de sueño
+        let retirement: Float = (65 * 365) * 24 // 65 años en horas
+        if (retirement - agetime) > 0 {
+            // Si no estás jubilado
+            if agetime < retirement {
+                agetime = (retirement - agetime)
+                agetime = agetime - (agetime / sleeptime) - (agetime / worktime)
+                // Si estás jubilado
+            } else {
+                agetime = Float(averageAge) - agetime
+                agetime = agetime - (agetime / sleeptime)
+            }
+        }
+        // Devuelve las horas de ocio estimadas
+        return Float((agetime + Float(averageAge)).roundToDecimal(1))
     }
 }
--- Livespan/ViewController.swift
+++ Livespan/ViewController.swift
@@ -10,19 +10,28 @@ extension Float {
 class ViewController: UIViewController {
 
     let data = UserDefaults.standard
+    @IBOutlet weak var AgeSlider: UISlider!
+    @IBOutlet weak var YearsOld: UITextView!
     @IBOutlet weak var WorkSlider: UISlider!
     @IBOutlet weak var WorkHours: UITextView!
     @IBOutlet weak var SleepSlider: UISlider!
     @IBOutlet weak var SleepHours: UITextView!
     @IBOutlet weak var CalcButton: UIButton!
+    var age: Float = 0
     var work: Float = 0
     var sleep: Float = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        data.set(0, forKey: "age")
         data.set(0, forKey: "work")
         data.set(0, forKey: "sleep")
     }
 
+    @IBAction func SaveAgealue(_ sender: UISlider) {
+        age = Float(sender.value.roundToDecimal(1))
+        YearsOld.text = "\(age) years"
+    }
+    
     @IBAction func SaveWorkValue(_ sender: UISlider) {
         work = Float(sender.value.roundToDecimal(1))
         WorkHours.text = "\(work) hours"
@@ -41,10 +50,8 @@ class ViewController: UIViewController {
     }
     
     func Calculate() {
+        data.set(age, forKey: "age")
         data.set(work, forKey: "work")
         data.set(sleep, forKey: "sleep")
-        print("cambio de pantalla")
-        self.navigationController?.pushViewController(CalculatedTimeViewController(), animated: true)
     }
 }
-
